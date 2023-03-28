b741ad555169ab016bfff521650a43b209f242bc
jye-aviatrix <93721751+jye-aviatrix@users.noreply.github.com>
2023-03-28 12:17:45 -0400

    Add private service connection IP allocation and create private conenction and enable export  custom route

--- main.tf
+++ main.tf
@@ -26,12 +26,10 @@ resource "google_compute_router" "edge_vpc_subnet_cloud_routers" {
   bgp {
     asn               = each.value.cr_asn
     advertise_mode    = "CUSTOM"
-    advertised_groups = [""]
-    dynamic "advertised_ip_ranges" {
-      for_each = each.value.advertised_ip_ranges
-      content {
-        range = advertised_ip_ranges.value
-      }
+    advertised_groups = []
+
+    advertised_ip_ranges {
+      range = each.value.private_service_connection_ip_range
     }
   }
   depends_on = [
@@ -41,22 +39,58 @@ resource "google_compute_router" "edge_vpc_subnet_cloud_routers" {
 
 # Provsion the first interface of CR
 resource "google_compute_router_interface" "cr_interface_1" {
-  for_each = var.edge_vpc_subnets
-  project  = var.project
-  region   = each.value.region
-  name       = "${google_compute_router.edge_vpc_subnet_cloud_routers[each.key].name}-1"
-  router     = google_compute_router.edge_vpc_subnet_cloud_routers[each.key].name
-  subnetwork = google_compute_subnetwork.edge_vpc_subnets[each.key].id
-  private_ip_address = cidrhost(each.value.ip_cidr_range, (pow(2,(32-tonumber(split("/",each.value.ip_cidr_range)[1])))-4))
+  for_each           = var.edge_vpc_subnets
+  project            = var.project
+  region             = each.value.region
+  name               = "${google_compute_router.edge_vpc_subnet_cloud_routers[each.key].name}-1"
+  router             = google_compute_router.edge_vpc_subnet_cloud_routers[each.key].name
+  subnetwork         = google_compute_subnetwork.edge_vpc_subnets[each.key].id
+  private_ip_address = cidrhost(each.value.ip_cidr_range, (pow(2, (32 - tonumber(split("/", each.value.ip_cidr_range)[1]))) - 4))
 }
 # Provsion the second interface of CR
 resource "google_compute_router_interface" "cr_interface_2" {
-  for_each = var.edge_vpc_subnets
-  project  = var.project
-  region   = each.value.region
-  name       = "${google_compute_router.edge_vpc_subnet_cloud_routers[each.key].name}-2"
-  router     = google_compute_router.edge_vpc_subnet_cloud_routers[each.key].name
-  subnetwork = google_compute_subnetwork.edge_vpc_subnets[each.key].id
-  private_ip_address = cidrhost(each.value.ip_cidr_range, (pow(2,(32-tonumber(split("/",each.value.ip_cidr_range)[1])))-3))
+  for_each           = var.edge_vpc_subnets
+  project            = var.project
+  region             = each.value.region
+  name               = "${google_compute_router.edge_vpc_subnet_cloud_routers[each.key].name}-2"
+  router             = google_compute_router.edge_vpc_subnet_cloud_routers[each.key].name
+  subnetwork         = google_compute_subnetwork.edge_vpc_subnets[each.key].id
+  private_ip_address = cidrhost(each.value.ip_cidr_range, (pow(2, (32 - tonumber(split("/", each.value.ip_cidr_range)[1]))) - 3))
 }
+
+
+# Private Service Connection IP Range allocations
+resource "google_compute_global_address" "private_service_connection_ip_range" {
+  for_each      = var.edge_vpc_subnets
+  project       = var.project
+  name          = "${var.edge_vpc_name}-psc-${each.value.region}"
+  address_type  = "INTERNAL"
+  purpose       = "VPC_PEERING"
+  network       = google_compute_network.edge_vpc.id
+  ip_version    = "IPV4"
+  address       = split("/", each.value.private_service_connection_ip_range)[0]
+  prefix_length = tonumber(split("/", each.value.private_service_connection_ip_range)[1])
+  # address       = each.value.private_service_connection_ip_range
+}
+
+# Equivlent to VPC -> Private Service Connection -> Private Connections to Services -> Select Google Cloud Platform as Connected service producer, and check allocated IP ranges.
+resource "google_service_networking_connection" "default" {
+  network                 = google_compute_network.edge_vpc.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [for k,v in google_compute_global_address.private_service_connection_ip_range: v.name]
+}
+
+# Make sure the VPC peering with the Private Service Connection will export custom route
+resource "google_compute_network_peering_routes_config" "private_service_access_generic" {
+  
+
+  project = var.project
+  peering = google_service_networking_connection.default.peering
+  network = google_compute_network.edge_vpc.name
+
+  import_custom_routes = false
+  export_custom_routes = true # We change this from the default to export our custom routes
+
+}
+
 
--- variables.tf
+++ variables.tf
@@ -13,22 +13,18 @@ variable "edge_vpc_subnets" {
   type = map
   default = {
     edge-us-central1 = {  # key name: eg edge-us-central1 will become subnet name
-        ip_cidr_range = "10.0.2.0/24"
-        region = "us-central1"
-        cr_asn = 65201
-        advertised_ip_ranges = [
-            "10.0.100.0/24", # must provide proper IPv4 CIDR notatiion in the format of nn.nn.nn.nn/nn
-            "10.0.101.0/24"
-        ]
+        ip_cidr_range = "10.0.2.0/24"  # Provide edge VPC subnet CIDR range for the first region
+        region = "us-central1" # Each subnet should be in the region mapping the gloabl service region, such as Cloud SQL.
+        cr_asn = 65201  # Each Cloud Router need it's unique ASN number
+
+        # must provide proper IPv4 CIDR notatiion in the format of nn.nn.nn.nn/nn
+        private_service_connection_ip_range = "10.0.100.0/24" # This is the range of IP allocated to Gloabl Services such as Cloud SQL. https://cloud.google.com/vpc/docs/configure-private-services-access?#allocating-range. Specify the IP range for this region only.
     }
     edge-us-west2 = {
-        ip_cidr_range = "10.0.3.0/24"
+        ip_cidr_range = "10.0.3.0/24" # Provide edge VPC subnet CIDR range for the second region
         region = "us-west2"
         cr_asn = 65202
-        advertised_ip_ranges = [
-            "10.0.102.0/24",
-            "10.0.103.0/24"
-        ]
+        private_service_connection_ip_range = "10.0.102.0/24"
     }
   }
 }
